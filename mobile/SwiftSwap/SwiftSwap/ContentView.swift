38979c0fe64f313b04836f04b2a57dd4ca3101b4
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-07-22 16:27:01 -0500

    feat: setup

---
+++ mobile/SwiftSwap/SwiftSwap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSwap
+//
+//  Created by Vaibhav Venkat on 7/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile/SwiftSwap/SwiftSwap/SwiftSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSwapApp.swift
+//  SwiftSwap
+//
+//  Created by Vaibhav Venkat on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
