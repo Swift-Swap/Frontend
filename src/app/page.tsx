8a1ba44b43700861f733c4fdfe4f996fa59add44
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-07-29 00:13:36 -0500

    feat: add some tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
         toast({
             title: "Unfinished website",
             description: "This website is still under construction. Please check back later!",
+            variant: "destructive",
             action: <ToastAction altText="Never show again" onClick={() => {
                 if (typeof localStorage === "undefined") return
                 localStorage.setItem("showed-unfinished-website", JSON.stringify(true))
@@ -32,10 +33,10 @@ export default function Home() {
         setShowed(true)
     })
     return (
-        <div className="flex flex-col justify-center items-center w-screen flex-1">
-            <div className="flex flex-col justify-start gap-8 items-center w-1/3 text-center mt-24">
-                <h1 className="text-6xl font-semibold"> Pave the way for an easy day! </h1>
-                <p className="text-muted-foreground">
+        <div className="flex flex-col justify-center items-center w-screen flex-1 fixed mt-32">
+            <div className="flex flex-col justify-start gap-8 items-center w-1/3 text-center">
+                <h1 className="text-7xl font-semibold"> Pave the way for an easy day! </h1>
+                <p className="text-muted-foreground text-lg">
                     A platform designed to help students trade parking spots on campus efficiently and conveniently. We aim to alleviate parking congestion by encouraging voluntary student spot exchanges.
                 </p>
                 <div className="flex gap-4">
@@ -54,7 +55,7 @@ export default function Home() {
                 <Image
                     className="mt-5"
                     src="https://lh3.googleusercontent.com/pw/AIL4fc8HD64g7VHf2szaXEpWXVo3sMz0vY2PEtYM05IRJivYi1ryqvxb4BDxYqxoonLzjr4RyGUVRoSq3bCrw-k2e2YhSt_063DfKCYBTFE0go5D8NxglYUdPIiotqm3q7Wfp1vtzE2dZ5JeFnJnf9ac3vvI4mwChgQQFXZdxTXMmKEi_cZ-O0S2AiRHuESwr8O3F9fMAwTA6sOFZKWXmlYBtRN7eKZFawxQCVCJIle4atvQhCh55cEWr_uSRsR1hQIfafVheRTuJ2WNMrUEqFDiLOY5QYP_MI6oOVv3MJMDJojRbGUy_MwOxH4A_Fr_tY5gQHBFuqrmzeukPCrAm2cpSK_E0ys8Dja_c5YZSa3ElTVhFhgd4wRHbGAdbi53q3PsH4bd4cMzK1cqU1RFPHENL1hVMBmB3xJUrQSAilOR7dlxkxLcnAUieXy6vkfBCnt3ysqyD3XHDx55jWOK515garJSs1k4Ds5KH4PB9jGUwNkuGBqiqg1Q3uJqLARepgFAF_fMHo2y6za9lNo3mvNgNriIxq4UkEU9eTWmMPYiGgB5b_YkH_7PUKyf3eDqbBHqrGp-cvy-6pUuIYQcplooCfqAeTy_KvMVtWUk5rYbjEycoFpsHMkDK32RJArGc8akVSGcIIgdFXzKZBuDAy4s0Wh0VTj9noMTT0bWq-FV07bA2SgjEt2l0LT0Fox6QTVthWe_o0zk2IYMmBqJXo5A5bG1OhGxw9XcLN-vHdp9oazGM9T5LYNzrqvwIEQmIaJUPkQMairOZijFbAyxUdtfF5vriHz_tExvee77WfzFoV3iK7qam8CAdFtzq_rTZNeEahsC8p41byAArGBQozAXUiG2ge-X4ojlM2aVEiGMClRzKsKIkijLiJUICNX-797JkwMS-nBqnIVTmhWbCuVkWrkqy-ajPR5X0biFdZ9QbhWkjHYJxMIlzVYhdyT7CR03FNWOiVMlpDzPnNZcLKiIQpMllTRAAj8BZjr43twVkl61nGQZ-u3JwRp1Z2CMSQ7libo=w660-h660-s-no?authuser=3"
-                    width={500} height={500} alt={"Image of car"} />
+                    width={400} height={400} alt={"Image of car"} />
             </div>
         </div>
     )
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,13 +9,14 @@ export default function Nav() {
     const [signedIn, setSignedIn] = React.useState(true)
     return (
         <nav className="p-4 px-8 border-b-2 border-b-foreground-50 flex gap-24 w-full justify-between bg-background items-center">
-            <Link href="/"> 
+            <Link href="/" className="flex items-center gap-4"> 
                 <Image 
                     width={50}
                     height={50}
                     className="rounded-full"
                     alt="SwiftSwap Logo"
                     src={`https://lh3.googleusercontent.com/pw/AIL4fc_8oTggrpwnfuxcYCVmUOEGlJBOmOUkYE4OwQFJ64mhs4XXaro5_ihBw6Rq6sxu5YpB7n5h3PQsv8oAQ_RyUywOHuTIy36bDAWUGcVq43A-FPSgXaiDx01ENT0YRKcVaYocHeaaNtCI1-y5H11gEcw=w480-h480-s-no?authuser=0`}/> 
+                <p className="text-md">SwiftSwap</p>
             </Link>
             <div className="flex px-4 gap-12 items-center">
                 <Link href="/about"> About </Link>
