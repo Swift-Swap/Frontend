ed7bb4ce8617224217e890d4d8208aae02457465
vibovenkat123 <vaibhav@vaibhavvenkat.com>
2023-07-31 00:49:26 -0500

    feat: add gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,10 +58,10 @@ export default function Home() {
   }
   return (
     <div
-      className={`flex flex-col justify-center items-center w-screen flex-1 p-2 `}
+      className={`flex flex-col justify-center items-center w-screen flex-1 p-2  bg-gradient-to-br from-primary-red to-primary-blue ${systemTheme === "dark" ? "via-background" : ""}`}
     >
       <div className="flex justify-center items-center gap-4 p-2 w-screen flex-1 flex-col bg-westlake bg-no-repeat bg-center bg-contain">
-        <div className="flex flex-col justify-center items-center gap-4 p-2 w-screen flex-1">
+        <div className={`flex flex-col justify-center items-center gap-4 p-2 w-screen flex-1`}>
           <div className="flex flex-col justify-center items-center gap-4 p-2 w-full md:w-3/4 lg:w-1/2 xl:w-1/3 flex-1">
             <h1 className="text-center text-5xl md:text-6xl lg:text-7xl font-bold">
               {" "}
